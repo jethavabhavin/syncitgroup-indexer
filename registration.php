653a9f0ed41348142ed2c78fe44329eab48a6e24
Bhavin Jethava <jethava.bhavin@gmail.com>
2021-02-04 00:51:57 +0530

    Add Module registration file

---
+++ registration.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * @category  Syncit Group Custom Contact Form
+ * @package   SyncitGroup_SgForm
+ * @copyright Copyright (c) 2021 Bhavin
+ * @author    Bhavin
+ */
+\Magento\Framework\Component\ComponentRegistrar::register(
+	\Magento\Framework\Component\ComponentRegistrar::MODULE,
+	'SyncitGroup_Indexer',
+	__DIR__
+);
